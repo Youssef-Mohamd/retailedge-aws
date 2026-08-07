5acce4003370bb6adb1148c55f4eedb0d8539189
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2026-08-08 01:46:12 +0300

    Allow controlled HTTPS egress for deployment agents

--- layers/layer-02-network/security-rules.tf
+++ layers/layer-02-network/security-rules.tf
@@ -44,15 +44,6 @@ resource "aws_vpc_security_group_egress_rule" "app_to_rds" {
   description                  = "Application to RDS"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "rds_from_app" {
-  security_group_id            = aws_security_group.rds.id
-  referenced_security_group_id = aws_security_group.app.id
-  from_port                    = 3306
-  to_port                      = 3306
-  ip_protocol                  = "tcp"
-  description                  = "MySQL from application"
-}
-
 resource "aws_vpc_security_group_egress_rule" "app_to_redis" {
   security_group_id            = aws_security_group.app.id
   referenced_security_group_id = aws_security_group.redis.id
@@ -60,6 +51,24 @@ resource "aws_vpc_security_group_egress_rule" "app_to_redis" {
   to_port                      = 6379
   ip_protocol                  = "tcp"
   description                  = "Application to Redis"
+}
+
+resource "aws_vpc_security_group_egress_rule" "app_https_outbound" {
+  security_group_id = aws_security_group.app.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+  description       = "HTTPS for CodeDeploy and AWS service endpoints when NAT is enabled"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "rds_from_app" {
+  security_group_id            = aws_security_group.rds.id
+  referenced_security_group_id = aws_security_group.app.id
+  from_port                    = 3306
+  to_port                      = 3306
+  ip_protocol                  = "tcp"
+  description                  = "MySQL from application"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "redis_from_app" {
