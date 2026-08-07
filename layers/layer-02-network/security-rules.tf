1e51bcafa5f993b018d56b90270c5d578114c390
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2026-08-08 01:39:58 +0300

    Define least privilege security rules

---
+++ layers/layer-02-network/security-rules.tf
@@ -0,0 +1,72 @@
+resource "aws_vpc_security_group_ingress_rule" "alb_https" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+  description       = "HTTPS from the internet"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_http" {
+  count             = var.allow_http_for_sandbox ? 1 : 0
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  description       = "HTTP for sandbox validation"
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_to_app" {
+  security_group_id            = aws_security_group.alb.id
+  referenced_security_group_id = aws_security_group.app.id
+  from_port                    = 8080
+  to_port                      = 8080
+  ip_protocol                  = "tcp"
+  description                  = "ALB to application"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "app_from_alb" {
+  security_group_id            = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.alb.id
+  from_port                    = 8080
+  to_port                      = 8080
+  ip_protocol                  = "tcp"
+  description                  = "Application traffic from ALB"
+}
+
+resource "aws_vpc_security_group_egress_rule" "app_to_rds" {
+  security_group_id            = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.rds.id
+  from_port                    = 3306
+  to_port                      = 3306
+  ip_protocol                  = "tcp"
+  description                  = "Application to RDS"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "rds_from_app" {
+  security_group_id            = aws_security_group.rds.id
+  referenced_security_group_id = aws_security_group.app.id
+  from_port                    = 3306
+  to_port                      = 3306
+  ip_protocol                  = "tcp"
+  description                  = "MySQL from application"
+}
+
+resource "aws_vpc_security_group_egress_rule" "app_to_redis" {
+  security_group_id            = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.redis.id
+  from_port                    = 6379
+  to_port                      = 6379
+  ip_protocol                  = "tcp"
+  description                  = "Application to Redis"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "redis_from_app" {
+  security_group_id            = aws_security_group.redis.id
+  referenced_security_group_id = aws_security_group.app.id
+  from_port                    = 6379
+  to_port                      = 6379
+  ip_protocol                  = "tcp"
+  description                  = "Redis from application"
+}
