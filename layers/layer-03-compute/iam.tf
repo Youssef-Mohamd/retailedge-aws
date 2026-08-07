c4be1246130d1237fc95c58ca1544c81b6036398
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2026-08-08 01:41:38 +0300

    Add the EC2 instance role for deployments

---
+++ layers/layer-03-compute/iam.tf
@@ -0,0 +1,34 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "app_instance" {
+  name               = "${var.project_name}-${var.environment}-app-instance"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read" {
+  role       = aws_iam_role.app_instance.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_read" {
+  role       = aws_iam_role.app_instance.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_instance_profile" "app" {
+  name = "${var.project_name}-${var.environment}-app-profile"
+  role = aws_iam_role.app_instance.name
+}
