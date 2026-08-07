10e7b952b6e4cafeb383908b48570b9f94c01841
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2026-08-08 01:46:19 +0300

    Add optional NAT settings

--- layers/layer-02-network/variables.tf
+++ layers/layer-02-network/variables.tf
@@ -4,43 +4,13 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "project_name" {
-  type    = string
-  default = "retailedge"
-}
-
-variable "environment" {
-  type    = string
-  default = "sandbox"
-}
-
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "availability_zones" {
-  type    = list(string)
-  default = ["us-east-1a", "us-east-1b"]
-}
-
-variable "public_subnets" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnets" {
-  type    = list(string)
-  default = ["10.0.11.0/24", "10.0.12.0/24"]
-}
-
-variable "database_subnets" {
-  type    = list(string)
-  default = ["10.0.21.0/24", "10.0.22.0/24"]
-}
-
-variable "allow_http_for_sandbox" {
-  type        = bool
-  description = "Allow HTTP to the ALB when the sandbox has no ACM certificate"
-  default     = true
-}
+variable "project_name" { type = string default = "retailedge" }
+variable "environment" { type = string default = "sandbox" }
+variable "vpc_cidr" { type = string default = "10.0.0.0/16" }
+variable "availability_zones" { type = list(string) default = ["us-east-1a", "us-east-1b"] }
+variable "public_subnets" { type = list(string) default = ["10.0.1.0/24", "10.0.2.0/24"] }
+variable "private_subnets" { type = list(string) default = ["10.0.11.0/24", "10.0.12.0/24"] }
+variable "database_subnets" { type = list(string) default = ["10.0.21.0/24", "10.0.22.0/24"] }
+variable "allow_http_for_sandbox" { type = bool default = true }
+variable "enable_nat_gateway" { type = bool default = false }
+variable "nat_gateway_count" { type = number default = 2 }
