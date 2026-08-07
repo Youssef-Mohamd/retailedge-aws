d14d0fc9db122d093f091fbe32d077b4eacc7bd9
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2026-08-08 01:42:07 +0300

    Define CI CD and monitoring inputs

---
+++ layers/layer-05-cicd/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" { type = string default = "us-east-1" }
+variable "project_name" { type = string default = "retailedge" }
+variable "environment" { type = string default = "sandbox" }
+variable "autoscaling_group_name" { type = string }
+variable "alb_arn_suffix" { type = string }
+variable "rds_instance_identifier" { type = string }
+variable "redis_replication_group_id" { type = string default = "" }
+variable "enable_redis_alarm" { type = bool default = false }
+variable "github_repository" { type = string description = "GitHub owner/repository, for example Youssef-Mohamd/retailedge-aws" }
+variable "notification_email" { type = string default = null nullable = true }
