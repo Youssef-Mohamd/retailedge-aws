6f2973475fad8ff81935a76421c1cd064e9d25ff
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2026-08-09 02:09:28 +0300

    remove unused ECR permissions

--- layers/layer-03-compute/iam.tf
+++ layers/layer-03-compute/iam.tf
@@ -18,11 +18,6 @@ resource "aws_iam_role" "app_instance" {
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "ecr_read" {
-  role       = aws_iam_role.app_instance.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
 resource "aws_iam_role_policy_attachment" "s3_read" {
   role       = aws_iam_role.app_instance.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
