6d724dc47dab0ecc3397ae8a0f38333118a1bb00
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2026-08-09 02:03:45 +0300

    make application port and health check configurable

--- layers/layer-03-compute/variables.tf
+++ layers/layer-03-compute/variables.tf
@@ -8,6 +8,8 @@ variable "alb_security_group_id" { type = string }
 variable "app_security_group_id" { type = string }
 variable "golden_ami_id" { type = string description = "AMI containing the application runtime used by the launch template" }
 variable "instance_type" { type = string default = "t3.small" }
+variable "app_port" { type = number default = 8080 description = "Port exposed by the application on the EC2 instances" }
+variable "health_check_path" { type = string default = "/" description = "HTTP path used by the ALB target group health check" }
 variable "min_size" { type = number default = 2 }
 variable "desired_capacity" { type = number default = 2 }
 variable "max_size" { type = number default = 10 }
