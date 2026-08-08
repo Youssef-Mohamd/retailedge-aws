26749ad024e9c2647476e359cd4922b5a82a11d8
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2026-08-09 02:04:15 +0300

    expose data identifiers for monitoring

--- layers/layer-04-data/outputs.tf
+++ layers/layer-04-data/outputs.tf
@@ -6,8 +6,16 @@ output "rds_port" {
   value = aws_db_instance.mysql.port
 }
 
+output "rds_instance_identifier" {
+  value = aws_db_instance.mysql.identifier
+}
+
 output "redis_primary_endpoint" {
   value = var.enable_redis ? aws_elasticache_replication_group.redis[0].primary_endpoint_address : null
+}
+
+output "redis_replication_group_id" {
+  value = var.enable_redis ? aws_elasticache_replication_group.redis[0].id : null
 }
 
 output "assets_bucket_name" {
